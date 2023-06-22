f494940a41e537eb5e2640c89fe23161ce9ed54c
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2023-06-22 23:36:27 +0700

    pokedexcard polish WIP

--- src/components/PokedexCardList.tsx
+++ src/components/PokedexCardList.tsx
@@ -5,22 +5,34 @@ type pokemonType = AllPokemonQueryQuery['getAllPokemon'][0]
 function PokedexCard({ pokemon }: { pokemon: pokemonType }) {
   return (
     <div className='flex flex-row w-full gap-4 px-4 py-4 bg-white border border-gray-200 rounded-md shadow-sm'>
-      <div className='flex'>
-        <div className='w-12 h-12 object-contain flex items-center justify-center'>
-          <img src={pokemon.sprite} alt={pokemon.key} className='max-h-full' />
-        </div>
-      </div>
       <div className='flex-auto'>
-        <div className='font-medium text-gray-900 capitalize'>
-          {pokemon.species.replace(/-/g, ' ')}
-        </div>
-        <div className='flex gap-2'>
-          {pokemon.types.map(type => (
-            <div key={type.name} className='text-xs text-gray-500 capitalize w-15 border px-2 py-1 rounded'>
-              {type.name}
+        <div className='flex justify-between'>
+          <div>
+            <div className='font-medium text-gray-900 capitalize'>
+              {pokemon.species.replace(/-/g, ' ')}
             </div>
-          ))}
+            <div className='flex gap-1 mt-1'>
+              {pokemon.types.map(type => (
+                <div
+                  key={type.name}
+                  className='text-xs text-gray-500 capitalize w-15 border px-2 py-1 rounded'
+                >
+                  {type.name}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='flex'>
+            <div className='w-12 h-12 object-contain flex items-center justify-center'>
+              <img
+                src={pokemon.sprite}
+                alt={pokemon.key}
+                className='max-h-full'
+              />
+            </div>
+          </div>
         </div>
+
         <div className='flex-auto grid grid-cols-[minmax(90px,1fr),minmax(50px,1fr)] text-sm text-gray-600 divide-y border-gray-50 [&>div]:py-1 mt-4'>
           <div>Total</div>
           <div className='text-right border-none'>{pokemon.baseStatsTotal}</div>
